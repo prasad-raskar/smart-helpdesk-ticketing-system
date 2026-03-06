4db739a353644f5cd5c17453b8058f958277a918
Prasad Raskar <prasadraskar330@gmail.com>
2026-03-07 00:32:55 +0530

    chore: Update build artifacts, add a log file, and modify docker-compose configuration.

--- Src/Helpdesk.Application/obj/Debug/net8.0/Helpdesk.Application.AssemblyInfo.cs
+++ Src/Helpdesk.Application/obj/Debug/net8.0/Helpdesk.Application.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Helpdesk.Application")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f53111916045ac50451eaa8edc9a9917d1181710")]
 [assembly: System.Reflection.AssemblyProductAttribute("Helpdesk.Application")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Helpdesk.Application")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
--- Src/Helpdesk.Infrastructure/obj/Debug/net8.0/Helpdesk.Infrastructure.AssemblyInfo.cs
+++ Src/Helpdesk.Infrastructure/obj/Debug/net8.0/Helpdesk.Infrastructure.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Helpdesk.Infrastructure")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f53111916045ac50451eaa8edc9a9917d1181710")]
 [assembly: System.Reflection.AssemblyProductAttribute("Helpdesk.Infrastructure")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Helpdesk.Infrastructure")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
